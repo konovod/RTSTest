5780fe7422f7a8e308c59712f04b29d4f06fe346
konovod <konovod@gmail.com>
2024-04-18 09:30:54 +0300

    turn to target (disabled default animation for now)

--- Assets/RTSFree/Scripts/ECS/Logic/Shooting.cs
+++ Assets/RTSFree/Scripts/ECS/Logic/Shooting.cs
@@ -37,7 +37,17 @@ namespace ECSGame
             var head = e.Get<GunHead>();
             var anim = e.Get<AimingAnimation>();
             var time = e.World.FirstComponent<UnityECSLink.GlobalTime>().Time;
-            head.v.localRotation = Quaternion.Euler(head.originalRotation.x, Mathf.PingPong(time * anim.seekSpeed, anim.rotateAngle * 2) - anim.rotateAngle, 1f);
+
+            if (e.Has<HasTarget>())
+            {
+                var target_position = e.Get<HasTarget>().v.Get<Position>().v;
+                Vector3 lookPos = target_position - head.v.position;
+                lookPos.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(lookPos);
+                head.v.rotation = Quaternion.Slerp(head.v.rotation, rotation, Time.deltaTime * anim.dampingSpeed);
+            }
+            // else
+            //     head.v.localRotation = Quaternion.Euler(head.originalRotation.x, Mathf.PingPong(time * anim.seekSpeed, anim.rotateAngle * 2) - anim.rotateAngle, 1f);
         }
     }
 
